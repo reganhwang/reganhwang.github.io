909b35747f34f6d50cc2b9c7c0fee72b2ff0cffc
reganhwang <77558168+reganhwang@users.noreply.github.com>
2022-03-16 16:10:48 +0000

    Add files via upload

---
+++ sendmail/myphp.php
@@ -0,0 +1,17 @@
+<?php
+
+$my_name = $_REQUEST['myName'];
+$my_email = $_REQUEST['myEmail'];
+$their_email = $_REQUEST['theirEmail'];
+$subject = $_REQUEST['subject'];
+$content = $_REQUESt['content'];
+
+$header = "From: $my_name"."<$my_email>\r\n";
+
+mail($to_email, $subject, $content, $header);
+
+echo 'Messege sent';
+
+
+
+?>
